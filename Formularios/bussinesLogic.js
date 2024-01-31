1f8ed5aa46b95387e0e487056ccb41d9604af760
Diego0024 <diegovtellez10@gmail.com>
2024-01-31 14:33:59 -0600

    Se agregan archivos formulario

---
+++ Formularios/bussinesLogic.js
@@ -0,0 +1,16 @@
+var listaUsuarios = [];
+
+function addUsertoSystem(pnombre, papellidopaterno, papellidomaterno, pusername, pbirthday, pemail, ppassword){
+
+    var newUser = {
+        nombre : pnombre,
+        apellidopaterno : papellidopaterno,
+        apellidomaterno : papellidomaterno,
+        username : pusername,
+        birthday : pbirthday,
+        email : pemail,
+        password : ppassword,
+
+    };
+    listaUsuarios.push(newUser);
+}
